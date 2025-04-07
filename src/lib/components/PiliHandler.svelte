3ca95685bc3a7b7f818fd6990bb731890afe5e1b
akhefale <adrianelias@protonmail.com>
2025-04-08 00:32:23 +0200

    dont animate out-of-viewport pilis

--- src/lib/components/PiliHandler.svelte
+++ src/lib/components/PiliHandler.svelte
@@ -1,5 +1,4 @@
 <script>
-
     import { onMount } from 'svelte'
     import { focus } from '$lib/utils/state.svelte'
 
@@ -36,12 +35,23 @@
     onMount(() => {
         const piliElements = document.querySelectorAll('.pili.living')
         let intervals = []
+        let observers = []
 
         console.log(piliElements)
 
         document.addEventListener('visibilitychange', () => {
             focus.focused = !document.hidden
         })
+
+        const isElementInViewport = (element) => {
+            const rect = element.getBoundingClientRect()
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            )
+        }
 
         piliElements.forEach(element => {
             const img = element.querySelector('img')
@@ -56,17 +66,26 @@
 
             const sleepDuration = Math.random() * 3 + 1
             const interval = setInterval(() => {
-                const randomId = ids[Math.floor(Math.random() * ids.length)]
-                img.src = `/asapili/${randomId}.svg`
+                if (isElementInViewport(element)) {
+                    const randomId = ids[Math.floor(Math.random() * ids.length)]
+                    img.src = `/asapili/${randomId}.svg`
+                }
             }, sleepDuration * 1000)
 
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    element.dataset.visible = entry.isIntersecting
+                })
+            })
+            
+            observer.observe(element)
+            observers.push(observer)
             intervals.push(interval)
         })
 
         return () => {
             intervals.forEach(interval => clearInterval(interval))
+            observers.forEach(observer => observer.disconnect())
         }
     })
-    
-
 </script>
